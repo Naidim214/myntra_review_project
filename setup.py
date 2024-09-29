f37fde489804302d6ba476c0985ab23e810bb425
Nadim214 <nadimgietcs@gmail.com>
2024-09-29 11:22:07 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author = 'nadim',
+    author_email='nadunadim1@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
